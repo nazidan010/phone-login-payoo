2a6bab9fadab78a6349a0fe157b535910fb82fbf
nazidan010 <nazidan010@gmail.com>
2024-09-22 09:19:14 +0600

    07

--- js/home.js
+++ js/home.js
@@ -11,4 +11,23 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // Check the confirm pin>>>>>>>
     const surePin=document.getElementById('input-pin').value;
     console.log(surePin);
+    //step 3: Verify pin number
+    //wrong way to pin number
+    if(surePin==='1234'){
+        console.log('add money to your account');
+        //step 4: get the current balance
+        const balance=document.getElementById('current-balance').innerText;
+        // console.log(balance);
+        //step 5: add and add money Input with balance
+        const addMoneyNumber=parseFloat(addMoney);
+        const addBalance=parseFloat(balance);
+        const currentBalance=addMoneyNumber+addBalance;
+        console.log(currentBalance);
+        //step 6: Update the balance in the UI/DOM
+        const addMoneyDisplay=document.getElementById('current-balance').innerText=currentBalance;
+    }
+    else{
+        alert('Failed to add money! Please try again')
+    }
+   
 });
